ebd8b6aca7d9626ccecd71a5d97119c5192d7bdb
huanmiaoi <47890849+huanmiaoi@users.noreply.github.com>
2024-08-30 17:44:55 +1000

    bugfix

---
+++ BoardGameProject/enum/EnumClass.cs
@@ -0,0 +1,16 @@
+
+namespace BoardGameProject
+{
+    public enum GameType
+    {
+        Notakto = 1,
+        Gomoku = 2
+    }
+
+    public enum GameMode
+    {
+        HumanVSHuman = 1,
+        ComputerVSHuman = 2
+
+    }
+}
